e38fece8b36c4490b0651b31c8534197639203c7
YoloSwagBot <bathcarson@gmail.com>
2023-11-29 11:42:32 -0800

    Initial project setup commit.

---
+++ aps.py
@@ -0,0 +1,7 @@
+# script to streamline setup for a new Android project
+# "CLEAN" Architecture
+# (1) Creates common directeories
+# (2) Option to add common dependencies(manually kept up to date for now)
+# (3) Option to create common modules
+
+echo "Hello APS!"
